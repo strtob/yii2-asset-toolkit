4c5b4a6898a1571474a3d1c09b23264e59e1c130
Tobi <streckel@gmx.de>
2024-10-09 21:27:43 +0200

    Update tools.js

--- rescource/js/tools.js
+++ rescource/js/tools.js
@@ -167,87 +167,79 @@ var initListener = function () {
             toastr.success(lajax.t('Value copied to clipboard: ' + dataValue));
         });
 
-$(document).on('click', '.showModal', function (event) {
-    console.log('showModal() listener');
-
-    // Set invoker
-    var invoker = $(this);
-
-    // Set modal title
-    var modalTitle = invoker.data('target-title') || 'Default Title';
-
-    // Load URL
-    var url = invoker.data('url');
-
-    // Modal size (optional) from data attribute
-    var modalSize = invoker.data('size') || '';  // small, large, extra-large, fullscreen, or '' for default size
-
-    // Confirm button class (optional) from data attribute
-    var confirmBtnClass = invoker.data('confirmbtnclass') || 'btn-success';  // Default is success
-
-    // Modal IDs for original and copy
-    var original = invoker.data('bs-target');
-    var copy = invoker.data('bs-target-copy');
-    var makeCopy = (copy !== undefined);
-    var elModal = $(original);
-
-    // Handle modal copying if necessary
-    if (makeCopy) {
-        console.log('Copy element ' + original + ' to element ' + copy);
-        elModal = $(copyElement(original, copy));
-    }
-
-    // Find modal components
-    var elModalTitle = elModal.find('.modal-title');
-    var elModalBody = elModal.find('.modal-body');
-    var elConfirmButton = elModal.find('#confirmButton'); // Assuming #confirmButton exists in the modal
-    var elModalDialog = elModal.find('.modal-dialog'); // Modal dialog element for resizing
-
-    // Set modal size
-    elModalDialog.removeClass('modal-sm modal-lg modal-xl modal-fullscreen'); // Remove existing size classes
-    if (modalSize === 'small') {
-        elModalDialog.addClass('modal-sm');
-    } else if (modalSize === 'large') {
-        elModalDialog.addClass('modal-lg');
-    } else if (modalSize === 'extra-large') {
-        elModalDialog.addClass('modal-xl');
-    } else if (modalSize === 'fullscreen') {
-        elModalDialog.addClass('modal-fullscreen');
-    }
-
-    // Set modal title
-    elModalTitle.html(modalTitle);
-
-    // Set confirm button class
-    if (elConfirmButton.length) {
-        elConfirmButton.removeClass().addClass('btn ' + confirmBtnClass);
-    }
-
-    // Check if URL is defined
-    if (typeof url !== 'undefined') {
-        console.log('Data URL ' + url + ' begin ajax load into modal ' + elModal.attr('id'));
-
-        // AJAX request to load content into the modal
-        $.ajax({
-            url: url,
-            type: 'GET',
-            success: function (response) {
-                // Set response content in the modal body
-                elModalBody.html(response);
-
-                // Show the modal after content is loaded
-                elModal.modal('show');
-            },
-            error: function (response) {
-                console.error("Error loading content into modal: ", response);
-                elModalBody.html('<div class="alert alert-danger">Failed to load content.</div>');
-                elModal.modal('show');
+        $(document).on('click', '.showModal', function (event) {
+            console.log('showModal() listener');
+        
+            // Set invoker
+            var invoker = $(this);
+        
+            // Set modal title
+            var modalTitle = invoker.data('target-title') || 'Default Title';
+        
+            // Load URL
+            var url = invoker.data('url');
+        
+            // Modal size (optional) from data attribute
+            var modalSize = invoker.data('size') || '';  // small, large, extra-large, fullscreen, or '' for default size
+        
+            // Confirm button class (optional) from data attribute
+            var confirmBtnClass = invoker.data('confirmbtnclass') || 'btn-success';  // Default is success
+        
+            // Get the generic modal element
+            var elModal = $('#genericModal');
+        
+            // Find modal components
+            var elModalTitle = elModal.find('.modal-title');
+            var elModalBody = elModal.find('.modal-body');
+            var elConfirmButton = elModal.find('#confirmButton'); // Assuming #confirmButton exists in the modal
+            var elModalDialog = elModal.find('.modal-dialog'); // Modal dialog element for resizing
+        
+            // Set modal size by adding/removing the appropriate Bootstrap classes
+            elModalDialog.removeClass('modal-sm modal-lg modal-xl modal-fullscreen'); // Remove existing size classes
+            if (modalSize === 'small') {
+                elModalDialog.addClass('modal-sm');
+            } else if (modalSize === 'large') {
+                elModalDialog.addClass('modal-lg');
+            } else if (modalSize === 'extra-large') {
+                elModalDialog.addClass('modal-xl');
+            } else if (modalSize === 'fullscreen') {
+                elModalDialog.addClass('modal-fullscreen');
+            }
+        
+            // Set modal title
+            elModalTitle.html(modalTitle);
+        
+            // Set confirm button class
+            if (elConfirmButton.length) {
+                elConfirmButton.removeClass().addClass('btn ' + confirmBtnClass);
+            }
+        
+            // Check if URL is defined and load the content via AJAX
+            if (typeof url !== 'undefined') {
+                console.log('Data URL ' + url + ' begin ajax load into modal ' + elModal.attr('id'));
+        
+                // AJAX request to load content into the modal
+                $.ajax({
+                    url: url,
+                    type: 'GET',
+                    success: function (response) {
+                        // Set response content in the modal body
+                        elModalBody.html(response);
+        
+                        // Show the modal after content is loaded
+                        elModal.modal('show');
+                    },
+                    error: function (response) {
+                        console.error("Error loading content into modal: ", response);
+                        elModalBody.html('<div class="alert alert-danger">Failed to load content.</div>');
+                        elModal.modal('show');
+                    }
+                });
+            } else {
+                console.error('No data-url defined for this event.');
             }
         });
-    } else {
-        console.error('No data-url defined for this event.');
-    }
-});
+        
 
 
     // Listener: in case modal will be closed

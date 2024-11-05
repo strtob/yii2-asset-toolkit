a2c1b8f10d0e2ccad92c54e3bf40641f2c6bbbcc
none <none@localhost.localdomain>
2024-11-05 12:09:42 +0100

    Bootstrap Icon Asset added

---
+++ src/BootstrapIcons.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace strtob\yii2AssetToolkit;
+
+use yii\web\AssetBundle;
+
+class BootstrapIconsAsset extends AssetBundle
+{
+    // The path to the toastr.js files in node_modules
+    public $sourcePath = '@vendor/strtob/yii2-asset-toolkit/rescource/node_modules/bootstrap-icons'; 
+
+    public $css = [
+        'bootstrap-icons.min.css',
+    ];
+
+    public $js = [       
+    ];
+
+    public $depends = [        
+    ];
+}

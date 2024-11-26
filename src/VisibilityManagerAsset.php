8e310c9908225b91f9efa4df856110f94e20604c
Tobi <streckel@gmx.de>
2024-11-26 16:58:41 +0100

    Update VisibilityManagerAsset.php
    update doc

--- src/VisibilityManagerAsset.php
+++ src/VisibilityManagerAsset.php
@@ -3,13 +3,12 @@
 namespace strtob\yii2AssetToolkit;
 
 use yii\web\AssetBundle;
-
 /**
  * VisibilityManagerAsset - An asset bundle for the VisibilityManager functionality in Yii2.
  *
  * This asset bundle includes the necessary JavaScript file for managing the visibility of form elements
- * based on dynamic user input, such as dropdown selections. It includes a custom JavaScript file (`VisibilityManagerAsset.js`)
- * to handle the visibility rules, which can be customized to show or hide form elements based on the selected value
+ * or their parent columns based on dynamic user input, such as dropdown selections. It includes a custom JavaScript file (`VisibilityManagerAsset.js`)
+ * to handle the visibility rules, which can be customized to show or hide form elements or their parent columns based on the selected value
  * of a dropdown or other event-driven triggers.
  *
  * Usage Example:
@@ -55,15 +54,23 @@ use yii\web\AssetBundle;
  *     }
  * ];
  *
- * const visibilityManager = new VisibilityManager(rules);
+ * const visibilityManager = new VisibilityManager({
+ *     rules: rules,
+ *     considerCols: true  // Apply visibility to parent columns (if set to false, visibility is applied to form elements directly)
+ * });
  *
  * In this example, when the dropdown with ID `#dropdown-id` changes:
  * - If the selected value is `1`, `#field-to-hide` will be hidden, and `#field-to-show` will be shown.
  * - If the selected value is `2`, the visibility is reversed for the fields.
  * - The `default` rule applies when no specific value is selected.
  * 
+ * The `considerCols` option allows you to decide whether the visibility is applied to the form element itself or its parent column. 
+ * - If `considerCols` is set to `true`, visibility will apply to the parent column (`.col-*`), hiding or showing the entire column.
+ * - If `considerCols` is set to `false` (default), visibility will apply only to the form element itself.
+ * 
  * @package strtob\yii2AssetToolkit
  */
+
 class VisibilityManagerAsset extends AssetBundle
 {
     /**

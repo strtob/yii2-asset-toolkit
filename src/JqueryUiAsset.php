21b2dd13207a487b29a8f44ee3d15b3ddb722659
Tobias Streckel <ts@finance-re.de>
2025-04-14 12:32:25 +0200

    further fix

--- src/JqueryUiAsset.php
+++ src/JqueryUiAsset.php
@@ -7,9 +7,6 @@ use yii\web\AssetBundle;
 
 class JqueryUiAsset extends AssetBundle
 {
-     // Pfad zu den jQuery UI Dateien in node_modules
-     public $sourcePath = '@vendor/strtob/yii2-asset-toolkit/rescource/node_modules/jquery-ui/dist';
-
      public $css = [
           'jquery-ui.min.css',
      ];
@@ -27,13 +24,13 @@ class JqueryUiAsset extends AssetBundle
           parent::init();
 
           // Pfad zu lokalem Development-Asset (z. B. app/strtob/yii2-asset-toolkit/...)
-          $localPath = Yii::getAlias('@app/strtob/yii2-asset-toolkit/rescource/node_modules/jquery-ui-dist');
+          $localPath = Yii::getAlias('@app/strtob/yii2-asset-toolkit/rescource/node_modules/jquery-ui/dist');
 
           if (is_dir($localPath)) {
-               $this->sourcePath = '@app/strtob/yii2-asset-toolkit/rescource/node_modules/jquery-ui-dist';
+               $this->sourcePath = '@app/strtob/yii2-asset-toolkit/rescource/node_modules/jquery-ui/dist';
           } else {
                // Fallback auf Vendor-Asset (Composer)
-               $this->sourcePath = '@vendor/strtob/yii2-asset-toolkit/rescource/node_modules/jquery-ui-dist';
+               $this->sourcePath = '@vendor/strtob/yii2-asset-toolkit/rescource/node_modules/jquery-ui/dist';
           }
      }
 }

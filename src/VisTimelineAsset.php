c37821a3834b760f5022f2456cd6529a3bb68a6c
Tobias Streckel <streckel@gmx.de>
2025-06-25 17:18:57 +0200

    Add VisTimelineAsset class for vis-timeline integration

---
+++ src/VisTimelineAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace strtob\yii2AssetToolkit;
+
+use yii\web\AssetBundle;
+
+class VisTimelineAsset extends AssetBundle
+{
+    // Pfad zum Verzeichnis mit vis-timeline (angepasst auf dein Setup)
+    public $sourcePath = '@vendor/strtob/yii2-asset-toolkit/resource/';
+
+    public $css = [
+        'node_modules/vis-timeline/styles/vis-timeline-graph2d.min.css',
+    ];
+
+    public $js = [
+        'node_modules/vis-timeline/standalone/umd/vis-timeline-graph2d.min.js',
+    ];
+
+    public $depends = [
+        'yii\web\YiiAsset',
+    ];
+}
